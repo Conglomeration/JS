42991512ae8280ce357a7d9c3a43e55efc3e1d18
YieldingCoder <yieldingexploiter@users.noreply.github.com>
2022-04-06 23:08:35 +0200

    chore: :coffin: Remove unnecesary comments

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,7 +1,5 @@
 /* eslint-disable object-curly-newline */
 module.exports = {
-  'extends': ['@norathefurry'], // use @norathefurry/base if you're dealing with vanilla JS
-  'rules': {
-    // your custom rules here
-  }
+  'extends': ['@norathefurry'],
+  'rules': {}
 };
